5a6a6196469abd1226cc005f481e37779270bea6
Shivam Raj Srivastav <shivamrajsrivastav38@gmail.com>
2025-07-23 22:50:07 +0530

    Update DB.js

--- Backend/DB.js
+++ Backend/DB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 function connectToDb() {
-    mongoose.connect('mongodb://localhost:27017/Shop'
+    mongoose.connect('Your Db address'
     ).then(() => {
         console.log('Connected to DB');
     }).catch(err => console.log(err));
